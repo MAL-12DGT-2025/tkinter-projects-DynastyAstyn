6897b8b5e0af983020d7fd3ec73a8df56a6b27c0
DynastyAstyn <astyn.tuiletufuga@marcellin.school.nz>
2025-03-04 15:13:29 +1300

    widgets added

--- temperature.py
+++ temperature.py
@@ -3,4 +3,11 @@ from tkinter import ttk
 
 root = tk.Tk()
 
+title = ttk.Label(root, text = "Temperature\nConverter").grid(row = 0)
+box = ttk.Entry(root).grid(row = 1)
+celcius_to_farenheit = ttk.Radiobutton(root, text = "C to F").grid(column = 0, row = 2)
+farenheit_to_celcius = ttk.Radiobutton(root, text = "F to C").grid(column = 1, row = 2)
+convert = ttk.Button(root, text = "Convert").grid(column = 0, row = 3)
+
+
 root.mainloop()
