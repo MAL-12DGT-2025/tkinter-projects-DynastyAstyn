ab1ffa675e45f9aaa866f8a791c3c80ce84f76c4
DynastyAstyn <astyn.tuiletufuga@marcellin.school.nz>
2025-03-19 13:30:03 +1300

    mostly done need to do radiobuttons, checkbox and remove

--- lemonadestand.py
+++ lemonadestand.py
@@ -6,24 +6,35 @@ from tkinter import ttk
 root = tk.Tk() 
 con_var = tk.IntVar()
 root.title('lemony') 
-constal = {
+
+##dictionary for the menu
+thing = {
 
     "Lemonade, $1":"1",
     "Strawberry Lemonade $1":"1",
     "Fizzy, $2":"2",
-    "Cookie, $2":"2",     
+    "Cookie, $2":"2"    
 
 }
-cartv = ""
-carv = ""
-def press(ops):
-    global carv
-    global cartv
-    menop = menu.get()
-    if ops == "add":
-        cartv =  f"{menop} + {cartv} "
-        carv =  str(eval(cartv))
-        cart.config(text = f"{cartv} = {carv}")
+
+##dictionary for extras
+other_thing = {
+
+    "Extra sugar +50¢" : "0.5",
+    "Extra lemon +20¢" : "0.2"
+
+}
+thingys_in_the_thing = []
+price = float()
+
+def press(thingels):
+    global price
+    if thingels == "add":
+        thingys_in_the_thing.append(menu.get())
+        thingys_in_the_thing.append(extra.get())
+        price += float(thing[menu.get()]) 
+        price += float(other_thing[extra.get()])
+        cart.config(text=f"Cart: {' with '.join(thingys_in_the_thing)},\nTotal: ${price:.2f}")
 
 
 ttk.Label(root, text = "Lemonade\nStand :p").grid(row = 0, column = 1) 
@@ -34,13 +45,13 @@ cart.grid(row = 6, column = 0)
 menu = ttk.Combobox(root, width = 20)
 menu.grid(row = 2 , column = 0)
 
-extra = ttk.Combobox(root, width = 25)
-extra.grid_remove()
+extra = ttk.Combobox(root, width = 20)
+extra.grid(row = 3, column = 0)
 
 extra['values'] = (
 
-    "Extra sugar +50c",                         
-    "Extra lemon +20c",            
+    "Extra sugar +50¢",                         
+    "Extra lemon +20¢",            
 
 )
 
@@ -64,16 +75,13 @@ button_pay = ttk.Button(root, text =  "Pay", command = lambda : press("pay"))
 button_pay.grid(row = 4 , column = 2, padx = 5)
 
 ##radiobuttons
-button_small = ttk.Radiobutton(root, text = "Small",  variable = con_var, value = 1)
+button_small = ttk.Radiobutton(root, text = "Small, -20¢",  variable = con_var, value = 1)
 button_small.grid(row = 2, column = 1, padx = 5)
 
-button_medium = ttk.Radiobutton(root, text = "Medium",  variable = con_var, value = 2)
-button_medium.grid(row = 3 , column = 1, padx = 5 )
+button_large = ttk.Radiobutton(root, text =  "Large +50¢",  variable = con_var, value = 3)
+button_large.grid(row = 3 , column = 1, padx = 5)
 
-button_large = ttk.Radiobutton(root, text =  "Large",  variable = con_var, value = 3)
-button_large.grid(row = 4 , column = 1, padx = 5)
-
-ice = ttk.Checkbutton(root, text =  "ice")
+ice = ttk.Checkbutton(root, text =  "ice")  ##ice checkbox
 ice.grid(row = 5 , column = 1)
 
 root.mainloop() 
